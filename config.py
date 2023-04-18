eaa9cb25b3197737ebbe2bf9f3a4dcbbe3d6df29
Faishka <131092403+Faishka@users.noreply.github.com>
2023-04-18 04:37:08 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+user_token = 'vk1.a.K6k1unO6NbcmZ7IcOTjRCHkdAb8U3PQCh-p8X4y-ggpI90JsdXskhUyyg5vn2f5VmgvsbKCSFErnfJkRgiX7cuFSOCZtIlN-y9sfQTA12pjlQ3Ny6w6jCj82rp0t6CLrCeY5b8vBYk2Go3KVMH_9dtmgb9gvC271r8AaKMQxBYduJneJcFtzs-QGnclHZ2gOrC9Pogu3mAkO6w7IJ157Nw' # токен пользователя
+group_token = 'vk1.a.ctODUEW2_7eJp_AoUm9zLTwKjVBtx5NYqA-sEzQc7qdXJU79cOxVk_HnMB5VZUcgNfUCsiknbpoisMI6dLxzwjO-YLVFBzcApP66Yk1sBpcAuZx9kYUmzz5dg0VkXm5Xm8mzUySvbSIZunp8Bm43tK_K11-ytyPcSqTUxAJVYsJvi1d4Ypg1eIrwjAPOficjzloMLxLSiCpIzUujapn8mg' # токен сообщества
+
+offset = 0 #сдвиг
+line = range(0, 1000) # последовательность для перебора найденных пользователей
+
+host = '127.0.0.1' 
+user = 'postgres'
+password = '123456'
+db_name = 'Testmain'
